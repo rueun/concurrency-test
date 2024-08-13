3824951ee28232d363f1f04e580d38043ba1ffde
ejshin-muhayu <ejshin@muhayu.com>
2024-08-13 12:39:03 +0900

    init: 프로젝트 기본 설정 추가

---
+++ src/test/java/com/example/concurrencytest/ConcurrencyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concurrencytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
